b6cc3549afaba042b49798743dbb650e25037bf7
YoungSun Shin <tlsdudtjs128@gmail.com>
2019-05-10 14:58:17 +0900

    #2 엔터키 사용하면 입력되도록 코드 구현

--- StringIO_TextView/Form1.Designer.cs
+++ StringIO_TextView/Form1.Designer.cs
@@ -40,6 +40,7 @@
             this.txtEdit.Name = "txtEdit";
             this.txtEdit.Size = new System.Drawing.Size(273, 21);
             this.txtEdit.TabIndex = 0;
+            this.txtEdit.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtEdit_KeyPress);
             // 
             // btnEdit
             // 
--- StringIO_TextView/Form1.cs
+++ StringIO_TextView/Form1.cs
@@ -47,5 +47,19 @@ namespace StringIO_TextView
             }
 
         }
+
+        private void txtEdit_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+
+                if (TextCheck())
+                {
+                    this.lblResult.Text = this.OrgStr + this.txtEdit.Text;
+
+                }
+
+            }
+        }
     }
 }
